4a6796417a2f7a137b1421434cc99aa9faec1fe2
Konstantin S. Uvarin <khedin@gmail.com>
2019-11-15 02:24:37 +0200

    API Add primitive loop detection

--- test/001-actor-loop.js
+++ test/001-actor-loop.js
@@ -10,10 +10,32 @@ console.log( Actor );
 describe( 'Actor', _=>{
     it( 'can call smth', done => {
         const trace = [];
-        const actor = new Actor({ foo : function(txt) { trace.push(txt) } });
+        const actor = new Actor()
+            .addCall( 'foo', function(txt) { trace.push(txt) } );
         actor.call( "foo", [ 42 ] );
         expect( trace ).to.deep.equal([42]);
         done();
+    });
+
+    it ('does not loop', done => {
+        let trace = [];
+        const actor = new Actor()
+            .addCall( 'foo', function(txt) {
+                 trace.push(['foo', txt]); this.call( 'bar', [ txt ] );
+            } )
+            .addCall( 'bar', function(txt) {
+                 trace.push(['bar', txt]); this.call( 'foo', [ txt ] );
+            } );
+
+        actor.call( "foo", [ 42 ] );
+        expect( trace ).to.deep.equal( [["foo", 42], ["bar", 42]] );
+
+        trace = [];
+        actor.call( "bar", [ 42 ] );
+        expect( trace ).to.deep.equal( [["bar", 42], ["foo", 42]] );
+
+        done();
+
     });
 
 });
