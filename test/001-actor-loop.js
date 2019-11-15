76a847bf902df313167e15fce96e1c46c8fe0c01
Konstantin S. Uvarin <khedin@gmail.com>
2019-11-15 03:14:10 +0200

    API Add callbacks; add mutual messaging test

--- test/001-actor-loop.js
+++ test/001-actor-loop.js
@@ -35,7 +35,35 @@ describe( 'Actor', _=>{
         expect( trace ).to.deep.equal( [["bar", 42], ["foo", 42]] );
 
         done();
+    });
 
+    it( 'can have mutual relationship', done => {
+        let trace = [];
+        const thomas = new Actor();
+        const jeremy = new Actor();
+
+        console.log("thomas="+thomas.id+"; jeremy="+jeremy.id);
+
+        thomas.onRecv = msg => console.log(msg);
+        jeremy.onRecv = msg => console.log(msg);
+
+        jeremy.onStart = _ => console.log("jeremy start");
+        jeremy.onStop  = _ => console.log("jeremy stop");
+
+        thomas.addCall( 'punch', str => {
+            trace.push( ['thomas', str] );
+            jeremy.call( 'punch', [ str ] );
+        });
+        jeremy.addCall( 'punch', str => {
+            trace.push( ['jeremy', str] );
+            thomas.call( 'punch', [ str ] );
+        });
+
+        jeremy.call( 'punch', [42] );
+
+        expect( trace ).to.deep.equal([['jeremy', 42], ['thomas', 42]]);
+
+        done();
     });
 
 });
