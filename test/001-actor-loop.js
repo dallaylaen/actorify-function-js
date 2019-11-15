83b1c534a92b881bed2ed24ecd9f4fcfbe895c97
Konstantin S. Uvarin <khedin@gmail.com>
2019-11-15 11:51:28 +0200

    TST Add onEnter/onLeave test

--- test/001-actor-loop.js
+++ test/001-actor-loop.js
@@ -20,7 +20,7 @@ describe( 'Actor', _=>{
 
         const scene = {};
         scene.thomas = actorify( n => { trace.push(['thomas', n]); scene.jeremy(n-1) } );
-        scene.jeremy = actorify( n => { trace.push(['jeremy', n]); scene.thomas(n-1) }, { max: 3 } );
+        scene.jeremy = actorify( n => { trace.push(['jeremy', n]); scene.thomas(n-1) }, { maxdepth: 3 } );
 
         scene.thomas(42);
         expect( trace ).to.deep.equal( [['thomas', 42], ['jeremy', 41]] );
@@ -29,6 +29,24 @@ describe( 'Actor', _=>{
         scene.jeremy(42);
         expect( trace ).to.deep.equal( [['jeremy', 42], ['thomas', 41], ['jeremy', 40]] );
 
+        done();
+    });
+
+    it( 'has onEnter and onLeave', done => {
+        const trace = [];
+        const obj = {};
+        obj.rec = actorify( x => obj.rec(x-1), {
+            maxdepth: 3,
+            onEnter: msg => trace.push( [ 'enter', msg.args[0] ] ),
+            onLeave: msg => trace.push( [ 'leave', msg.args[0] ] ),
+        } );
+
+        obj.rec(42);
+        expect( trace ).to.deep.equal([
+            ['enter', 42], ['leave', 42],
+            ['enter', 41], ['leave', 41],
+            ['enter', 40], ['leave', 40],
+        ]);
         done();
     });
 
